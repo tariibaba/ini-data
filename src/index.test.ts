3384be6cafdd582d0ecfce22e83e2ef1926db420
Ayibatari Ibaba <tariibabadev@gmail.com>
2021-12-16 12:54:46 +0100

    Write tests

---
+++ src/index.test.ts
@@ -0,0 +1,21 @@
+import { parse } from './index';
+
+describe('Parser', () => {
+  it('works', () => {
+    const data = `
+key1 = value1
+[section1]
+key2 = value2
+[section2.section]
+
+key3 = value3
+`;
+    const obj = parse(data);
+    const expected = {
+      key1: 'value1',
+      section1: { key2: 'value2' },
+      ['section2.section']: { key3: 'value3' },
+    };
+    expect(obj).toEqual(expected);
+  });
+});
